63631262bda11fed1017ba1837800a3c85f7becf
SylviXu <x.xu78@lse.ac.uk>
2023-12-26 02:04:19 +0800

    Complete initial processing of requested data.

--- Code Script.R
+++ Code Script.R
@@ -1,117 +1,143 @@
 # This is a code script for assignment 4.
-# Install and load packages.
-#install.packages('spotifyr')
-library(spotifyr)
-library(tidyverse)
-library(knitr)
-library(httr)
-library(RSelenium)
+
+# Load packages
 library(rvest)
 library(xml2)
+library(dplyr)
+library(spotifyr)
 library(jsonlite)
-#set_config(use_proxy(url = "Proxy_Add_Here", port = 8090))
+library(httr)
+library(tidyverse)
 
-## Scrape the 100 greatest artists ranked by Rolling Stone Magazine.==============================
+## Step 1: scrape the 100 greates artists data from Rolling Stone webpage.======
 
-# Set up the driver and navigate to Rolling Stone's ranking page.
-rD <- rsDriver(browser=c("firefox"), verbose = F, port = 4567L, chromever = NULL) 
-driver <- rD[["client"]]
+# Scrape the first page's 50 artists.
+# Navigate to the webpage.
 website <- "https://www.rollingstone.com/music/music-lists/100-greatest-artists-147446/"
-driver$navigate(website)
 
-# Create a scraping function to collect all the rankings and names of the artists presented on the page.
-rank_scrape <- function() {
-  src <- driver$getPageSource() # Get the source code of the page.
-  
-  result_html <- read_html(src[[1]]) # Get the html.
-  
-  rank_table <- data.frame() # Create a blank list to storage final data.
-  
-  # Get the rank data from the page.
-  rank <- src %>%
-    html_nodes("span.c-gallery-vertical-album__number") %>%
-    html_text() %>%
-    as.numeric()
-  
-  # Get the names of the artists.
-  names <- src %>%
-    html_nodes("h2.c-gallery-vertical-album__title") %>%
-    html_text()
-  
-  # Create new rows in the final table.
-  rank_table$Rank <- rank
-  rank_table$Name <- names
-  
-  # Return the result of the function.
-  return(rank_table)
-}
+# Read the page's html.
+res <- read_html(website)
 
-# Since there are only 50 artists on one single page, we need to scrape the 1-50 and 51-100 artists separately and then combine them into one table.
-# First, scrape the 51-100 artists' data.
-rank_51_100 <- rank_scrape()
-# Then find the "load more" button and click, navigating to next page.
-load_button <- driver$findElement(using = "xpath", value = "/html/body/div[4]/main/div[2]/div[1]/div/article/div[3]/div[2]/div[2]")
-load_button$clickElement()
-# Scrape the 1-50 artists' data.
-rank_1_50 <- rank_scrape()
+# Get the 51-100 artists' data.
+rank_51_100 <- res %>%
+  html_nodes("h2") %>%
+  html_text() %>%
+  as.data.frame() %>%
+  head(50)
+colnames(rank_51_100) <- "Name" #Rename the column.
+rank_51_100$Rank <- 100:51 #Add ranking numbers.
 
-## Get artist details from Spotify API. ================================================
-# Set up my client id and secret from a local file.
+# Navigate to the next page which contains 1-50 artists' data.
+website2 <- "https://www.rollingstone.com/music/music-lists/100-greatest-artists-147446/the-band-2-88489/"
+res2 <- read_html(website2)
+rank_1_50 <- res2 %>%
+  html_nodes("h2") %>%
+  html_text() %>%
+  as.data.frame() %>%
+  head(50)
+colnames(rank_1_50) <- "Name" #Rename the column.
+rank_1_50$Rank <- 50:1 #Add ranking numbers.
+
+# Combine two data frames into one single table.
+greatest_100_artists <- bind_rows(rank_51_100, rank_1_50) %>%
+  arrange(Rank)
+
+## Step 2: retrieve more details for analysis from Spotify API.================
+
+# Set up my Spotify client id and secret by reading from a local ".env" file.
 readRenviron("E:/文件/LSE/MY472/spotify id&secret.env")
 id <- Sys.getenv("id")
 secret <- Sys.getenv("secret")
 Sys.setenv(SPOTIFY_CLIENT_ID = id)
 Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
+
+# Set the access token.
 access_token <- get_spotify_access_token()
 
-# Function to get all artists' Spotify ids.
-get_spotify_artist_ids <- function(artist_names) {
-  artist_ids <- sapply(artist_names, function(name) {
-    artist <- spotifyr::get_artist(name)
-    if (nrow(artist) > 0) {
-      return(artist$id[1])
-    } else {
-      return(NA)
-    }
-  })
-  return(artist_ids)
+# Create a function to retrieve an artist's spotify id from api.
+get_spotify_id <- function(artist_name) {
+  # To avoid timeout issues, I create a retry mechanism system here, which contains a loop combined with tryCatch and Sys.sleep to make repeated attempts at the API call.
+  attempt <- 1 # Set the minimum number of retry attempts.
+  max_attempts <- 5 # Set the maximum number of retry attempts.
+  # Write a loop to retrieve data while avoiding timeout errors.
+  while (attempt <= max_attempts) {
+    try({
+      # Set up the api endpoint.
+      search_url <- paste0('https://api.spotify.com/v1/search?q=', URLencode(artist_name), '&type=artist&limit=1')
+      # Get response.
+      response <- GET(search_url, timeout(30), add_headers(Authorization = paste('Bearer', access_token)))
+      if (status_code(response) == 200) {
+        search_results <- content(response, as = "parsed", type = "application/json")
+        # Here I add an if sentence to deal with potential NA values.
+        if (length(search_results$artists$items) > 0) {
+          return(search_results$artists$items[[1]]$id)
+        } else {
+          return(NA)
+        }
+      }
+    }, silent = TRUE)
+    attempt <- attempt + 1
+    Sys.sleep(5)  # pause for 5 seconds before retrying
+  }
+  return(NA)
 }
 
-# Function to Get Multiple Artists' Details.
-get_multiple_artists_details <- function(artist_ids, token) {
-  valid_ids <- paste(na.omit(artist_ids), collapse = ",")
-  url <- paste0("https://api.spotify.com/v1/artists/?ids=", valid_ids)
-  response <- httr::GET(url, add_headers(`Authorization` = paste("Bearer", token)))
-  details <- jsonlite::fromJSON(httr::content(response, "text", encoding = "UTF-8"))
-  return(details$artists)
+# Improve the function into another function that retrieve a list of artists' spotify id.
+get_ids <- function(rank) {
+  for (i in 1:nrow(rank)) {
+    name <- rank$Name[i]
+    rank$id[i] <- get_spotify_id(name)
+  }
+  return(rank)
 }
 
-# Write a function to update my data.
-update_data <- function(rank){
-  rank$spotify_id <- get_spotify_artist_ids(rank$Name)
-  # Fetch the details
-  artist_details <- get_multiple_artists_details(rank$spotify_id, access_token)
+# Get all the artists' spotify ids and add them into my dataframe.
+greatest_100_artists <- get_ids(greatest_100_artists)
+
+# Retrieve followers and popularity index.====================================================
+
+# Function to get an artist's followers and popularity index.
+get_artist_details <- function(id, token) {
+  # Set the url endpoint.
+  url <- paste0("https://api.spotify.com/v1/artists/?ids=", id)
   
-  # Assuming artist_details_1_50 is a list of artist details
-  # Create a data frame from the details
-  details_df <- data.frame(
-    spotify_id = sapply(artist_details, function(x) x$id),
-    followers = sapply(artist_details, function(x) x$followers$total),
-    popularity = sapply(artist_details, function(x) x$popularity)
-  )
-  # Merge the new details with the original data frame
-  rank_update <- merge(rank, details_df, by = "spotify_id")
-  return(rank_update)
+  # To avoid timeout issues mentioned before, here I create a retry mechanism as well.
+  attempt <- 1 # Set the minimum number of retry attempts.
+  max_attempts <- 5  # Set the maximum number of retry attempts.
+  while (attempt <= max_attempts) {
+    response <- tryCatch({
+      # Get API response.
+      GET(url, timeout(30), add_headers(`Authorization` = paste("Bearer", token)))
+    }, error = function(e) { NULL })
+    
+    # Check if the request was successful
+    if (!is.null(response) && status_code(response) == 200) {
+      # Retrieve text details from a json file we got.
+      details <- fromJSON(content(response, "text", encoding = "UTF-8"))
+      # Extract the followers and popularity data.
+      followers <- details$artists$followers$total
+      popularity <- details$artists$popularity
+      
+      # Create a new data frame to storage the retrieved data.
+      return(data.frame(followers = followers, popularity = popularity))
+    } else {
+      message(sprintf("Attempt %d failed. Retrying in %d seconds...", attempt, attempt))
+      Sys.sleep(attempt)  # Exponential back-off
+      attempt <- attempt + 1
+    }
+  }
+  # If all attempts failed, output warning message and return NA values.
+  warning("All attempts failed.")
+  return(data.frame(followers = NA, popularity = NA))
 }
 
-rank_1_50 <- update_data(rank_1_50)
-rank_51_100 <- update_data(rank_51_100)
+# Mapping the artists dataframe to add followers and popularity details.
+for (i in 1:nrow(greatest_100_artists)) {
+  id <- greatest_100_artists$id[i]
+  df <- get_artist_details(id, access_token)
+  greatest_100_artists$followers[i] <- df$followers[1]
+  greatest_100_artists$popularity[i] <- df$popularity[1]
+}
 
-# Finally, combine them into one table.
-greatest_100_artists <- bind_rows(rank_1_50, rank_51_100)
-
-print(rank_51_100)
-print(rank_1_50)
-print(greatest_100_artists)
-# ========================================================================================
-
+# Review the first few rows of the updated dataframe.
+head(greatest_100_artists)
